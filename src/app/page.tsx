7b2091a022d901dbf873c636df01009968ccdff8
vzxh <wkq7740@gmail.com>
2025-06-27 17:32:50 +0800

    refine UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { AppFooter } from "@/components/AppFooter";
 import { Contact } from "@/components/Contact";
 import { QA } from "@/components/QA";
-import { Section1 } from "@/components/Section1";
 import { Section2 } from "@/components/Section2";
 import { Section3 } from "@/components/Section3";
 import { Section4 } from "@/components/Section4";
